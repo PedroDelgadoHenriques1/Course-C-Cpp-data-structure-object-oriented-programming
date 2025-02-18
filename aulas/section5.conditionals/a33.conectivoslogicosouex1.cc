7e20a840006e4e9013d4414411ff202a29b71ef0
Pedro Delgado Henriques <pedrodelgadohenriques1@gmail.com>
2025-02-18 16:29:45 -0300

    mais alguns exercicios de if else

---
+++ aulas/section5.conditionals/a33.conectivoslogicosouex1.cc
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    //FAÇA UM PROGRAMA QUE LEIA 2 NÚMEROS INTEIROS E IMPRIMA NA TELA SE ALGUM FOR PAR. 
+    // DICA: PARA SEPARAR OPERAÇÕES DE COMPARAÇÃO USE PARÊNTESES.
+    
+
+    return 0;
+}
