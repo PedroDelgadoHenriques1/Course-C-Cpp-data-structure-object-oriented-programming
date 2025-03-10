40f73393df46c60340656affdb03987e5612b401
Pedro Delgado Henriques <pedrodelgadohenriques1@gmail.com>
2025-03-10 11:14:03 -0300

    terminando section 18 ordenação

--- aulas/section18.datastructure-ordering/a139. Quick Sort.cc
+++ aulas/section18.datastructure-ordering/a139. Quick Sort.cc
@@ -31,9 +31,8 @@ void quick_sort(int vetor[TAM], int inicio, int fim){
         while(vetor[esq] < pivo){
             esq = esq + 1;
         }
-
+ 
         while(vetor[dir] > pivo){
-            dir = dir - 1;
             dir = dir - 1;
         }
  
@@ -70,7 +69,7 @@ int main(){
  
     int vetor[TAM] = {10,9,8,7,6,5,4,3,2,1};
  
-    quick_sort(vetor, 0, TAM-1);
+    quick_sort(vetor, 0, TAM);
  
     imprimeVetor(vetor);
  
